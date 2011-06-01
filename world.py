947ea12c05bcfccb9e6d4d843b51721d3d16cc83
Tom Coxon <tom.c.coxon@googlemail.com>
2011-06-01 22:17:37 +0100

    Break up camera class into a hierarchy.

--- world.py
+++ world.py
@@ -21,25 +21,55 @@ FOV_360 = 2*math.pi
 # Speed of movement of the camera
 WALK_SPEED = 4.0 # m/s
 
-class camera(object):
+class world_object(object):
     def __init__(self, world):
         self.world = world
+        (self.x, self.y, self.z) = (0.0,0.0,0.0)
+        (self.rot_x, self.rot_y) = (0.0, 0.0) # No rot_z for now...
+    def advance(self, t):
+        pass
+    def move_forward(self, dist, x, z):
+        # dist can be -ve to move backward
+        x = x + dist * math.sin(self.rot_x)
+        z = z + dist * math.cos(self.rot_x)
+        return (x,z)
+    def move_sideways(self, dist, x, z):
+        x = x - dist * math.cos(self.rot_x)
+        z = z + dist * math.sin(self.rot_x)
+        return (x,z)
+    def move_up(self, dist, y):
+        y = y + dist
+        return y
+    def try_move(self,x,y,z):
+        moved = False
+        # TODO what if the edge type is ET_PORTAL_TORUS
+        if x != self.x and self.world.legal_x(x):
+            self.x = x
+            moved = True
+        if z != self.z and self.world.legal_z(z):
+            self.z = z
+            moved = True
+        if y != self.y and self.world.legal_y(y):
+            self.y = y
+            moved = True
+        #if moved:
+        #    print "pos = (%f, %f, %f)" % (self.x, self.y, self.z)
 
-        self.rot_x = 0.0
-        self.rot_y = 0.0
-        self.x = 0.5
-        self.y = 1.5
-        self.z = 0.5
+class physical_object(world_object):
+    # TODO implement physics
+    pass
+
+class entity(physical_object):
+    pass
+
+class camera(world_object):
+    def __init__(self, world):
+        world_object.__init__(self, world)
         self.fov = FOV_DEFAULT
-
         # For transitions:
         self.prev_fov = self.fov
         self.target_fov = self.fov
         self.fov_trans_count = 0
-
-        # Keyboard input:
-        self.keys_down = set()
-
     def advance(self, t):
         # Advance FOV transition:
         if self.fov != self.target_fov:
@@ -52,6 +82,30 @@ class camera(object):
         else:
             self.fov = (self.prev_fov + self.fov_trans_count *
                 (self.target_fov-self.prev_fov)/1000)
+    def fov_x(self):
+        return self.fov
+    def fov_y(self):
+        return self.fov if self.fov <= math.pi else math.pi
+    def toggle_fov(self):
+        # Toggles FOV only if there is no current transition!
+        if self.fov == FOV_DEFAULT:
+            self.target_fov = FOV_360
+        elif self.fov == FOV_360:
+            self.target_fov = FOV_DEFAULT
+    
+
+class player_character(entity, camera):
+    def __init__(self, world, x, y, z):
+        entity.__init__(self, world)
+        camera.__init__(self, world)
+
+        (self.x, self.y, self.z) = (x, y, z)
+
+        self.keys_down = set()
+
+    def advance(self, t):
+        camera.advance(self, t)
+        entity.advance(self, t)
 
         # Move the camera if relevant key is down:
         dist = WALK_SPEED * t / 1000.0
@@ -70,38 +124,6 @@ class camera(object):
             y = self.move_up(-dist, y)
         self.try_move(x,y,z)
 
-    def move_forward(self, dist, x, z):
-        # dist can be -ve to move backward
-        x = x + dist * math.sin(self.rot_x)
-        z = z + dist * math.cos(self.rot_x)
-        return (x,z)
-    def move_sideways(self, dist, x, z):
-        x = x - dist * math.cos(self.rot_x)
-        z = z + dist * math.sin(self.rot_x)
-        return (x,z)
-    def move_up(self, dist, y):
-        y = y + dist
-        return y
-    def try_move(self,x,y,z):
-        moved = False
-        if x != self.x and self.world.legal_x(x):
-            self.x = x
-            moved = True
-        if z != self.z and self.world.legal_z(z):
-            self.z = z
-            moved = True
-        if y != self.y and self.world.legal_y(y):
-            self.y = y
-            moved = True
-        #if moved:
-        #    print "pos = (%f, %f, %f)" % (self.x, self.y, self.z)
-
-    def fov_x(self):
-        return self.fov
-
-    def fov_y(self):
-        return self.fov if self.fov <= math.pi else math.pi
-    
     def on_key_up(self, key, _x, _y):
         self.keys_down.remove(key)
 
@@ -109,10 +131,7 @@ class camera(object):
         self.keys_down.add(key)
 
         if key == 'o':
-            if self.fov == FOV_DEFAULT:
-                self.target_fov = FOV_360
-            elif self.fov == FOV_360:
-                self.target_fov = FOV_DEFAULT
+            camera.toggle_fov(self)
 
     def on_click(self, button, state, x, y):
         pass
@@ -143,9 +162,9 @@ class world(object):
         # What to display at the end of the world
         self.edge_type = ET_WALL
 
-        self.camera = camera(self)
-        self.controllables = [self.camera]
-        self.entities = [self.camera]
+        self.player = player_character(self, 0.5, 1.5, 0.5)
+        self.camera = self.player
+        self.entities = [self.player]
 
     def init_cldata(self, ctx):
         self.grid_clbuf = cl.Buffer(ctx,
@@ -188,20 +207,14 @@ class world(object):
         for e in self.entities:
             e.advance(t)
 
-    ## These three functions (send_*) send input information to
-    ## controllable items in the world (e.g. the camera)
     def send_key_down(self, key, x, y):
-        for c in self.controllables:
-            c.on_key_down(key, x, y)
+        self.player.on_key_down(key, x, y)
 
     def send_key_up(self, key, x, y):
-        for c in self.controllables:
-            c.on_key_up(key, x, y)
+        self.player.on_key_up(key, x, y)
 
     def send_click(self, button, state, x, y):
-        for c in self.controllables:
-            c.on_click(button, state, x, y)
+        self.player.on_click(button, state, x, y)
 
     def send_mouse_motion(self, x, y):
-        for c in self.controllables:
-            c.on_mouse_motion(x, y)
+        self.player.on_mouse_motion(x, y)
